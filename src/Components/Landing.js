f7af4a63668b4a5c5e53832ad9496a28ad922721
dewanhimanshu <himanshudewan786@gmail.com>
2019-04-02 22:37:35 +0530

    inital commit

---
+++ src/Components/Landing.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+    <div>
+      <div>
+          Nav Bar
+      </div>
+      <div>
+          Add 
+      </div>
+    </div>
+    );
+  }
+}
+
+export default App;
